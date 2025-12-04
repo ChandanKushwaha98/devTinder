86c43e68553fbe37d6db62f4eadacb91d9153720
ChandanKushwaha98 <ckushwaha981@gmail.com>
2025-12-04 14:18:10 +0530

    Add feed endpoint to fetch user profiles while excluding certain connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectRequest');
+const User = require('../models/user');
 
 
 const USER_SAFE_DATA = 'firstName lastName age about photoUrl skills'
@@ -56,6 +57,50 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
         });
     }
 
+});
+
+userRouter.get('/feed', userAuth, async (req, res) => {
+
+    //interested , accepted, ignored, rejected and his own requests should not be in the feed
+    try {
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }]
+        }).select('fromUserId toUserId')
+
+        const hideusersFromFeed = new Set();
+        connectionRequests.forEach((request) => {
+            hideusersFromFeed.add(request.fromUserId.toString());
+            hideusersFromFeed.add(request.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideusersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+
+            ]
+        })
+            .select(USER_SAFE_DATA)
+            .skip(skip)
+            .limit(limit);
+
+        res.status(200).json({
+            message: "User profile fetched successfully",
+            data: users
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Error fetching user profile",
+            error: error.message
+        });
+    }
 });
 
 
